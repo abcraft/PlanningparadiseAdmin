5d58ec5a6f948f98eab10d94b522d1d7673f8a78
AbdulRahim <76427217+abcraft@users.noreply.github.com>
2022-01-04 02:34:56 +0530

    Publishing Admin Dashboard Designing Changes From AbdulRahim
    Publishing Admin Dashboard Designing Changes From AbdulRahim

---
+++ Areas/Admin/Controllers/BlogsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PlanningParadiseAdmin.Data;
+using PlanningParadiseAdmin.Models;
+
+namespace PlanningParadiseAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public BlogsController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        // GET: Admin/Blogs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Blogs.ToListAsync());
+        }
+
+        // GET: Admin/Blogs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // GET: Admin/Blogs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Blogs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Blog_Img,Blog_Heading,BlogShort_Detail,BlogLong_Detail,IsActive")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                if (HttpContext.Request.Form.Files.Count() > 0)
+                {
+                    var f = HttpContext.Request.Form.Files[0];
+                    string wwwRootPath = _hostEnvironment.WebRootPath;
+                    string fileName = Path.GetFileNameWithoutExtension(f.FileName);
+                    string extension = Path.GetExtension(f.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    blog.Blog_Img = fileName;
+                    string path = Path.Combine(wwwRootPath + "/Images/DestinationWedding", fileName);
+                    FileStream fileStream1 = new FileStream(path, FileMode.Create);
+                    f.CopyTo(fileStream1);
+                }
+
+                _context.Add(blog);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blog);
+        }
+
+        // GET: Admin/Blogs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Admin/Blogs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Blog_Img,Blog_Heading,BlogShort_Detail,BlogLong_Detail,IsActive")] Blog blog)
+        {
+            if (id != blog.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blog);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogExists(blog.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blog);
+        }
+
+        // GET: Admin/Blogs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // POST: Admin/Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            _context.Blogs.Remove(blog);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogExists(int id)
+        {
+            return _context.Blogs.Any(e => e.ID == id);
+        }
+    }
+}
