4ad7154944f19ef1c391c906186605211c8df5ed
Al-hamed-Mohammed <mohd.vtt@gmail.com>
2022-01-03 19:33:17 +0530

    AboutUs Testimonials Banner
    AboutUs Testimonials Banner

--- Areas/Admin/Controllers/HomesController.cs
+++ Areas/Admin/Controllers/HomesController.cs
@@ -245,34 +245,140 @@ namespace PlanningParadiseAdmin.Areas.Admin.Controllers
             return View(home);
         }
 
-        // GET: Admin/Homes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Banner()
         {
-            if (id == null)
+            var banner = await _context.Banner.FindAsync(1);
+            if (banner == null)
             {
                 return NotFound();
             }
-
-            var home = await _context.Home
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (home == null)
-            {
-                return NotFound();
-            }
-
-            return View(home);
+            return View(banner);
         }
-
-        // POST: Admin/Homes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        [HttpPost]
+        public async Task<IActionResult> Banner(Banner banner)
         {
-            var home = await _context.Home.FindAsync(id);
-            _context.Home.Remove(home);
+            var b1 = await _context.Banner.FirstOrDefaultAsync(e => e.ID == 1);
+            if (banner.AboutBanner_Img == null)
+                banner.AboutBanner_Img = b1.AboutBanner_Img;
+            if (banner.ServiceBanner_Img == null)
+                banner.ServiceBanner_Img = b1.ServiceBanner_Img;
+            if (banner.DestinationBanner_Img == null)
+                banner.DestinationBanner_Img = b1.DestinationBanner_Img;
+            if (banner.GalleryBanner_Img == null)
+                banner.GalleryBanner_Img = b1.GalleryBanner_Img;
+            if (banner.BlogBanner_Img == null)
+                banner.BlogBanner_Img = b1.BlogBanner_Img;
+            if (banner.FaqBanner_Img == null)
+                banner.FaqBanner_Img = b1.FaqBanner_Img;
+            if (banner.ContactBanner_Img == null)
+                banner.ContactBanner_Img = b1.ContactBanner_Img;
+           
+            foreach (var item in HttpContext.Request.Form.Files)
+            {
+                if (item.Length > 0)
+                {
+                    var f = item;
+                    string wwwRootPath = _hostEnvironment.WebRootPath;
+                    string fileName = Path.GetFileNameWithoutExtension(f.FileName);
+                    string extension = Path.GetExtension(f.FileName);
+                    fileName = item.Name + DateTime.Now.ToString("yymmssfff") + extension;
+                    string path = Path.Combine(wwwRootPath + "/Images/Banner", fileName);
+                    if (item.Name == "AboutBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.AboutBanner_Img);
+
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.AboutBanner_Img = fileName;
+                    }
+                    if (item.Name == "ServiceBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.ServiceBanner_Img);
+
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.ServiceBanner_Img = fileName;
+                    }
+                    if (item.Name == "DestinationBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.DestinationBanner_Img);
+
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.DestinationBanner_Img = fileName;
+                    }
+                    if (item.Name == "GalleryBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.GalleryBanner_Img);
+
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.GalleryBanner_Img = fileName;
+                    }
+                    if (item.Name == "FaqBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.FaqBanner_Img);
+
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.FaqBanner_Img = fileName;
+                    }
+                    if (item.Name == "BlogBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.BlogBanner_Img);
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.BlogBanner_Img = fileName;
+                    }
+                    if (item.Name == "ContactBanner_Img")
+                    {
+                        var path1 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\Banner", b1.ContactBanner_Img);
+                        if (System.IO.File.Exists(path1))
+                        {
+                            System.IO.File.Delete(path1);
+                        }
+                        banner.ContactBanner_Img = fileName;
+                    }
+                  
+                    FileStream fileStream1 = new FileStream(path, FileMode.Create);
+                    f.CopyTo(fileStream1);
+
+                }
+
+            }
+            b1.AboutBanner_Heading = banner.AboutBanner_Heading;
+            b1.AboutBanner_Img = banner.AboutBanner_Img;
+            b1.ServiceBanner_Heading = banner.ServiceBanner_Heading;
+            b1.ServiceBanner_Img = banner.ServiceBanner_Img;
+            b1.DestinationBanner_Heading = banner.DestinationBanner_Heading;
+            b1.DestinationBanner_Img = banner.DestinationBanner_Img;
+            b1.GalleryBanner_Heading = banner.GalleryBanner_Heading;
+            b1.GalleryBanner_Img = banner.GalleryBanner_Img;
+            b1.BlogBanner_Heading = banner.BlogBanner_Heading;
+            b1.BlogBanner_Img = banner.BlogBanner_Img;
+            b1.FaqBanner_Heading = banner.FaqBanner_Heading;
+            b1.FaqBanner_Img = banner.FaqBanner_Img;
+            b1.ContactBanner_Heading = banner.ContactBanner_Heading;
+            b1.ContactBanner_Img = banner.ContactBanner_Img;
+
+
+            _context.Entry(b1).State = EntityState.Modified;
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return View(banner);
         }
+
 
         private bool HomeExists(int id)
         {
